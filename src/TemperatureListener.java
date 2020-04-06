a4e5009fd71876dfd06c5800b1ee7c3e738517be
MarquiseG <meriem.karoune@gmail.com>
2020-04-06 16:38:00 +0200

    first commit

---
+++ src/TemperatureListener.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface TemperatureListener extends Remote
+{
+    void temperatureChanged(double temperature) throws RemoteException;
+}
